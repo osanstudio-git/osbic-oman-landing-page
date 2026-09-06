570761fc4538fbd05781aaad87dbe7c4e3594d55
osanstudio-git <marketing@osanstudio.net>
2026-09-06 18:01:23 +0400

    updated image size

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,11 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  devToolbar: {
+    enabled: false,
+  },
   vite: {
     plugins: [tailwindcss()],
   },
 });
+
